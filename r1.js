53f20164942b9f5524c4bbd1169257e5e20f2f0c
Michael Angerman <stormasm@gmail.com>
2017-12-16 11:08:39 -0800

    adding in another example

---
+++ r1.js
@@ -0,0 +1,5 @@
+require('babel-core/register');
+
+var test = require('./rtest1.js');
+
+test('aa', 'b', 22, 43);
---
+++ rtest1.js
@@ -0,0 +1,12 @@
+function test1(foo, ...args) {
+    let [bar, baz] = args;
+
+    for(let arg of args) {
+        let bar = 22;
+        console.log(`arg: ${arg} bar: ${bar}`);
+    }
+
+    console.log(`done: ${bar} :: ${baz}`);
+}
+
+module.exports = test1;
