52c35c784ac543a0064245138290e2d988f083a5
Michael Angerman <stormasm@gmail.com>
2017-12-18 11:05:46 -0800

    md2

--- md2.js
+++ md2.js
@@ -1,20 +1,12 @@
 require('babel-core/register');
-var isJSON = require('is-json');
 
-var md = require('markdown-it')();
+// var md = require('markdown-it')();
 // assumes you have markdown-it src code available
-// var md = require('./../markdown-it')();
+var md = require('./../markdown-it')();
 
-var result = md.parse('hola michael');
-resultjson = JSON.stringify(result, null, 2);
-
-console.log(isJSON(resultjson));
-// console.log(resultjson)
-
-//result = md.render('_hola_')
-//console.log(result);
+var tokens = md.parse('hola michael');
 
 var Renderer = require('./../markdown-it/lib/renderer');
-myrenderer = new Renderer();
-result = myrenderer.render(result)
-console.log(result)
+token_renderer = new Renderer();
+html = token_renderer.render(tokens)
+console.log(html)
