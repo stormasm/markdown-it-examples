e6aa5aee629dc5602d69491f397f2ae3ca9571b3
Michael Angerman <stormasm@gmail.com>
2017-12-18 19:26:23 -0800

    md3

---
+++ md3.js
@@ -0,0 +1,16 @@
+require('babel-core/register');
+
+var Renderer = require('./../markdown-it/lib/renderer');
+var md = require('markdown-it')();
+// assumes you have markdown-it src code available
+// var md = require('./../markdown-it')();
+
+var fs   = require('fs');
+var path = require('path');
+var source = fs.readFileSync(path.join(__dirname, './sample1.md'), 'utf8')
+console.log(md.render(source));
+
+var tokens = md.parse(source)
+token_renderer = new Renderer();
+html = token_renderer.render(tokens)
+console.log(html)
